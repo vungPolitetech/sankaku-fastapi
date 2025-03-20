a7bbf3c7ec964e39846af89daa75927732d3989f
vungbt <user.email>
2025-03-20 16:55:38 +0700

    fix: revert code

--- app/main.py
+++ app/main.py
@@ -2,7 +2,6 @@ from fastapi import FastAPI, Response, Request
 import os
 from dotenv import load_dotenv
 from fastapi.middleware.cors import CORSMiddleware
-from fastapi.responses import StreamingResponse
 
 load_dotenv()
 app = FastAPI()
@@ -17,14 +16,13 @@ app.add_middleware(
 )
 
 SOURCE_DIR = os.getenv("SOURCE_DIR")
-CHUNK_SIZE = int(os.getenv("CHUNK_SIZE", 1))  # Default 1MB
 
 @app.get("/stream/{filename:path}")
 async def stream_video(filename: str, request: Request):
     file_path = os.path.join(SOURCE_DIR, filename)
     if not os.path.exists(file_path):
         return Response("File not found", status_code=404)
-
+    
     range_header = request.headers.get("range", None)
     file_size = os.path.getsize(file_path)
 
@@ -36,18 +34,12 @@ async def stream_video(filename: str, request: Request):
     end = int(end) if end else file_size - 1
     end = min(end, file_size - 1)
 
+    chunk_size = 6 * 1024 * 1024  # 6MB
     content_length = end - start + 1
 
-    def iter_video():
-        with open(file_path, "rb") as f:
-            f.seek(start)
-            remaining = content_length
-            while remaining > 0:
-                chunk = f.read(min(CHUNK_SIZE, remaining))
-                if not chunk:
-                    break
-                yield chunk
-                remaining -= len(chunk)
+    with open(file_path, "rb") as f:
+        f.seek(start)
+        video_data = f.read(content_length)
 
     headers = {
         "Content-Range": f"bytes {start}-{end}/{file_size}",
@@ -55,6 +47,8 @@ async def stream_video(filename: str, request: Request):
         "Content-Length": str(content_length),
         "Content-Type": "video/mp4",
         "Access-Control-Allow-Origin": "*",  # Add CORS header directly
+        "Content-Disposition": "inline",
     }
 
-    return StreamingResponse(iter_video(), status_code=206, headers=headers)
+    return Response(video_data, status_code=206, headers=headers)
+
